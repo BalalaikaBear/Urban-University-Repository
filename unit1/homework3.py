e4d6f80229e0de037ab59b5d52af51e897dc3251
BalalaikaBear <101048037+BalalaikaBear@users.noreply.github.com>
2024-06-18 19:03:16 +0300

    Add files via upload

---
+++ unit1/homework3.py
@@ -0,0 +1,12 @@
+name = "Oleg"
+print("Name:", name)
+
+age = 25
+print("Age:", age)
+
+add_age = 3
+age = int(age) + int(add_age)
+print("Age after", add_age, "year(s):", age)
+
+is_student = True
+print("Is student:", is_student)
