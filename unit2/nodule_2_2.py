efa78f4ab566cad56b9207aaafde3a25bd40d4e1
BalalaikaBear <eightes@yandex.ru>
2024-06-23 12:59:24 +0300

    None

--- unit2/nodule_2_2.py
+++ unit2/nodule_2_2.py
@@ -2,24 +2,24 @@ first = int(input("Введите первое число:"))
 second = int(input("Введите второе число:"))
 third = int(input("Введите третье число:"))
 
-# вариант 1
+# вариант 1 - в лоб
 if first == second and first == third:
-    print(3)
+    print("Одинаковых чисел:", 3)
 elif first == second or first == third or second == third:
-    print(2)
+    print("Одинаковых чисел:", 2)
 else:
-    print(0)
+    print("Одинаковых чисел:", 0)
 
-# вариант 2
+# вариант 2 - через множество
 t = {first, second, third}
 if len(t) == 1:
-    print(3)
+    print("Одинаковых чисел:", 3)
 elif len(t) == 2:
-    print(2)
+    print("Одинаковых чисел:", 2)
 else:
-    print(0)
+    print("Одинаковых чисел:", 0)
 
-# вариант 3
+# вариант 3 - через множество + оптимизированный if (поскольку вариант 0 самый вероятный)
 t = {first, second, third}
-if len(t) == 3: print(0)
-else: print(4-len(t))
+if len(t) == 3: print("Одинаковых чисел:", 0)
+else: print("Одинаковых чисел:", 4-len(t))
