a0fc89f6012594088849b7be944748560025677e
BalalaikaBear <eightes@yandex.ru>
2024-07-06 19:24:42 +0300

    None

--- unit3/module_3_5.py
+++ unit3/module_3_5.py
@@ -3,7 +3,7 @@ def get_multiplied_digits(number):
     str_number = str(number)  # преобразоваие числа в строку
     first = int(str_number[0])  # первая цифра строки
     if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))  # при приобразовании строки в числа первые нули убираются
+        return first * get_multiplied_digits(int(str_number[1:]))  # при преобразовании строки в числа первые нули убираются
     else:
         return first
 
