c902b0dcb65823e9a548d6681cb7552d215a77fb
BalalaikaBear <eightes@yandex.ru>
2024-07-08 18:40:46 +0300

    None

--- unit3/module3hard.py
+++ unit3/module3hard.py
@@ -22,18 +22,30 @@
 
 def summator(*input):
     input = list(input)
-    print(input[0])
+    print(input)
     if not (type(data_structure[0]) == float
         or type(data_structure[0]) == int):  # если 1-й элемент не число
         input = [0] + list(input)  # вставить число в начало списка
                                    # 1-е число - сумма
     sum = input[0]
-    if len(input) == 1:
+    if len(input) == 1 and (type(input) == int or type(input) == float):  # последнее число? -> завершить
         return sum
     else:
+        # основная логика
         for thing in input[1:]:
             if type(thing) == list or type(thing) == tuple or type(thing) == set:  # если последовательность
-                return summator()
+                return summator(sum, thing)
+            elif isinstance(thing, str):  # если str
+                return sum + len(thing)
+            elif type(thing) == float or type(thing) == int:  # если число
+                return sum + thing
+            elif isinstance(thing, dict):  # если словарь
+                for i, j in thing.items():
+                    return summator(sum, i, j)
+
+
+def test(*input):
+    print(input)
 
 data_structure = [
     [1, 2, 3],
@@ -43,4 +55,5 @@ data_structure = [
     ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
+test(0, data_structure)
 print(summator(data_structure))
