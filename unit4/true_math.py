9519d6566ce48ec4bea8f3fe152e2812cc586d95
BalalaikaBear <eightes@yandex.ru>
2024-07-09 20:03:06 +0300

    None

--- unit4/true_math.py
+++ unit4/true_math.py
@@ -1,9 +1,11 @@
 # from math import inf
 
 def divide(first, second):
-    if second == 0 and first >= 0:
+    if second == 0 and first > 0:
         return float("+inf")  # или return inf при подключенной библиотеки
     elif second == 0 and first < 0:
         return float("-inf")
+    elif first == 0 and second == 0:
+        return 1
     else:
         return first / second
