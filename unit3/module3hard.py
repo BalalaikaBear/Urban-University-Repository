f5e10c65d87547fa450bc7a1b9e13b3b9a58e3cc
BalalaikaBear <eightes@yandex.ru>
2024-07-13 19:33:08 +0300

    None

--- unit3/module3hard.py
+++ unit3/module3hard.py
@@ -1,5 +1,4 @@
 def summator(*n):
-    _sum = 0
 
     # отладка
 #    print("Summator:", sum, type(n), n)
@@ -7,24 +6,21 @@ def summator(*n):
     if len(n) == 1:  # если последовательность размером в 1 элемент
         n = n[0]  # раскрыть последовательность
 
-    if isinstance(n, (int, float)):  # если первый элемент число, занести число в sum
-        _sum = n
-
     # отладка
 #    print("Summator:", sum, type(n), n)
 
     if isinstance(n, (int, float)):  # если число
-        return _sum
+        return n
     elif isinstance(n, str):  # если строка
-        return _sum + len(n)
+        return len(n)
     elif isinstance(n, (list, tuple, set)):  # если последовательность
         n = list(n)  # преобразование set в list (без этой строки ошибка, поскольку set is not subscriptable)
         if len(n) == 0:
-            return _sum
+            return 0
         else:
-            return _sum + summator(n[0]) + summator(n[1:])
+            return summator(n[0]) + summator(n[1:])
     elif isinstance(n, dict):  # если словарь
-        return _sum + summator(list(n.items()))
+        return summator(list(n.items()))
 
 
 data_structure = [
