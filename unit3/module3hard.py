c4334b0fec96c05f3d0de63d9d869abf9f09660e
BalalaikaBear <eightes@yandex.ru>
2024-07-13 14:49:01 +0300

    None

--- unit3/module3hard.py
+++ unit3/module3hard.py
@@ -1,5 +1,7 @@
 def summator(*n):
     sum = 0
+
+    # отладка
     #print("Summator:", sum, type(n), n)
 
     if len(n) == 1 and (type(n[0]) != float or type(n[0]) != int):  # если последовательность размером в 1 элемент
@@ -8,7 +10,7 @@ def summator(*n):
     if type(n) == float or type(n) == int:  # если первый элемент число, занести число в sum
         sum = n
 
-
+    # отладка
     print("Summator:", sum, type(n), n)
 
     if isinstance(n, int) and isinstance(n, float):  # если число
@@ -16,16 +18,15 @@ def summator(*n):
     elif isinstance(n, str):  # если строка
         return sum + len(n)
     elif isinstance(n, list) or isinstance(n, tuple) or isinstance(n, set):  # если последовательность
+        n = list(n)  # преобразование set в list (без этой строки ошибка)
         if len(n) == 0:
             return sum
         else:
-            if type(n[0]) != int or type(n[0]) != float:
-                return sum + summator(n[1:])  # если n[0] == "NoneType"
-            else:
-                return sum + summator(n[0]) + summator(n[1:])
+            return sum + summator(n[0]) + summator(n[1:])
     elif isinstance(n, dict):  # если словарь
-        for i, j in n.items():
-            return sum + summator(i, j)
+        return sum + summator(list(n.items()))
+    else:  # без него не работает, не понимаю почему (проверка на NoneType?)
+        return sum
 
         #return n[0] + summator(n[1:])
 
@@ -37,4 +38,7 @@ data_structure = [
     ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
+check = 2, "Tarja", {"": 11, "fd": 3}, (), [1, 2, "four", {"": 11, "fd": 3}]
+
+#print(summator(check))
 print(summator(data_structure))
