589de0bae7eadb2a56697fbb7571d18d2ada057a
BalalaikaBear <eightes@yandex.ru>
2024-07-13 14:55:49 +0300

    None

--- unit3/module3hard.py
+++ unit3/module3hard.py
@@ -1,34 +1,33 @@
 def summator(*n):
-    sum = 0
+    sum_ = 0
 
     # отладка
-    #print("Summator:", sum, type(n), n)
+#    print("Summator:", sum, type(n), n)
 
     if len(n) == 1 and (type(n[0]) != float or type(n[0]) != int):  # если последовательность размером в 1 элемент
         n = n[0]  # раскрыть последовательность
 
     if type(n) == float or type(n) == int:  # если первый элемент число, занести число в sum
-        sum = n
+        sum_ = n
 
     # отладка
-    print("Summator:", sum, type(n), n)
+#    print("Summator:", sum, type(n), n)
 
     if isinstance(n, int) and isinstance(n, float):  # если число
-        return sum + n
+        return sum_ + n
     elif isinstance(n, str):  # если строка
-        return sum + len(n)
+        return sum_ + len(n)
     elif isinstance(n, list) or isinstance(n, tuple) or isinstance(n, set):  # если последовательность
         n = list(n)  # преобразование set в list (без этой строки ошибка)
         if len(n) == 0:
-            return sum
+            return sum_
         else:
-            return sum + summator(n[0]) + summator(n[1:])
+            return sum_ + summator(n[0]) + summator(n[1:])
     elif isinstance(n, dict):  # если словарь
-        return sum + summator(list(n.items()))
+        return sum_ + summator(list(n.items()))
     else:  # без него не работает, не понимаю почему (проверка на NoneType?)
-        return sum
+        return sum_
 
-        #return n[0] + summator(n[1:])
 
 data_structure = [
     [1, 2, 3],
