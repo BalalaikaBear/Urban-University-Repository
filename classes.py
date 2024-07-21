3070d7a68a7b23dc5897a321cf56b721b06b1a38
BalalaikaBear <eightes@yandex.ru>
2024-07-21 18:02:34 +0300

    None

--- classes.py
+++ classes.py
@@ -4,6 +4,12 @@ class Human:  # создание собственного типа данных
         self.age = age
 #       self.say_info() - вызов функции при создании класса
 
+    def __del__(self):  # деструктор - срабатывает при удалении объекта из памяти (при ЗАВЕРШЕНИИ программы также СРАБАТЫВАЕТ!)
+        print(f"__del__: {self.name} ушёл")
+
+    def __len__(self):  # при вызове функции len будет возвращаться возраст объекта
+        return self.age
+
     def say_info(self):  # создание метода в классе
         print(f"Привет, меня зовут {self.name}, мне {self.age}")
 
@@ -16,5 +22,6 @@ print("1:", type(man))  # собственный тип данных
 print("2:", man == woman, man is woman, id(man), id(woman))  # каждый объект из класса уникальный
 print("3:", type(woman.__getstate__()), woman.__getstate__())  # отображение всех атрибутов класса в виде словаря
 print("4:", man.name, forty_seven.name)  # обращение к данным
+print("5:", len(man))  # обращение к функции __len__ в классе
 
 man.say_info()  # вызов метода для определенного класса
