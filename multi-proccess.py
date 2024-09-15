2bdae4c87252baa95992637bc98e7a794d84440e
BalalaikaBear <eightes@yandex.ru>
2024-09-15 13:24:29 +0300

    None

--- multi-proccess.py
+++ multi-proccess.py
@@ -3,8 +3,8 @@ import numpy as np
 import math
 from datetime import datetime
 
-# With multiprocessing: 0:00:22.157997 [1.5e+03 0.0e+00 1.0e+00]
-# Without multiprocessing: 0:00:26.870035 [1.5e+03 0.0e+00 1.0e+00]
+# With multiprocessing: 0:00:02.448328 [787.88645946 576.77291892   1.        ]
+# Without multiprocessing: 0:00:03.094564 [787.88645946 576.77291892   1.        ]
 
 
 sqrt3 = math.sqrt(3)
@@ -18,13 +18,13 @@ def matrix_basis(points):
 
 
 if __name__ == '__main__':  # необходимо, чтобы мультипроцесс не запускал сам себя
-    positions = [(x, y) for x in range(3000) for y in range(3000)]
+    positions = [(x, y) for x in range(1000) for y in range(1000)]
 
     start = datetime.now()
-    # использование нескольких процессов (ядер)
-    with multiprocessing.Pool(processes=4) as pool:  # <processes> - количество процессов (ядер)
+    # использование нескольких процессов
+    with multiprocessing.Pool(processes=4) as pool:  # <processes> - количество процессов
 
-        # аналогично map, но разбиением на процессы (ядра)
+        # аналогично map, но разбиением на процессы
         A = pool.map(matrix_basis, positions)
         # 1-й аргумент - функция
         # 2+ аргументы - итерируемые объекты, поступающие в функцию
