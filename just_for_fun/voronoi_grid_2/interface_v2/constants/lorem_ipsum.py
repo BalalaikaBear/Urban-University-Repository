ba14294c3e355f4bab6b29aa967b5edd3c85c451
BalalaikaBear <eightes@yandex.ru>
2024-11-09 22:37:56 +0300

    None

---
+++ just_for_fun/voronoi_grid_2/interface_v2/constants/lorem_ipsum.py
@@ -0,0 +1,115 @@
+from enum import StrEnum
+
+class Loren(StrEnum):
+    Ipsum = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et "
+             "dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip "
+             "ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore "
+             "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia "
+             "deserunt mollit anim id est laborum.")
+    Hipster = ("Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat tbh semiotics artisan synth "
+               "stumptown gastropub cornhole celiac swag. Brunch raclette vexillologist post-ironic glossier ennui "
+               "XOXO mlkshk godard pour-over blog tumblr humblebrag. Blue bottle put a bird on it twee prism biodiesel "
+               "brooklyn. Blue bottle ennui tbh succulents.")
+    Zombie = ("Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata "
+              "corpora quaeritis. Summus brains sit, morbo vel maleficia? De apocalypsi gorger omero undead survivor "
+              "dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi "
+              "dentevil vultus comedat cerebella viventium.")
+    Bacon = ("Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare "
+             "ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha "
+             "ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round "
+             "jerky brisket pastrami shank.")
+    Cupcake = ("Cupcake ipsum dolor. Sit amet marshmallow topping cheesecake muffin. Halvah croissant candy canes "
+               "bonbon candy. Apple pie jelly beans topping carrot cake danish tart cake cheesecake. Muffin danish "
+               "chocolate soufflé pastry icing bonbon oat cake. Powder cake jujubes oat cake. Lemon drops tootsie "
+               "roll marshmallow halvah carrot cake.")
+    Delorean = ("Just say anything, George, say what ever's natural, the first thing that comes to your mind. Take "
+                "that you mutated son-of-a-bitch. My pine, why you. You space bastard, you killed a pine. You do? "
+                "Yeah, it's 8:00. Hey, McFly, I thought I told you never to come in here. Well it's gonna cost you. "
+                "How much money you got on you?")
+    Space = ("Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: "
+             "to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man "
+             "has gone before. Many say exploration is part of our destiny, but it’s actually our duty to future "
+             "generations and their quest to ensure the survival of the human species.")
+    Busey = ("Busey ipsum dolor sit amet. Have you urinated? Have you drained your bladder? Are you free? Because "
+             "if you haven't it will only come out later. I'm giving you some information that your bodily fluids "
+             "may penetrate your clothing fibre's without warning.It's OK to get Rib-grease on your face, because "
+             "you're allowing people to see that you're proud of these ribs.")
+    Samuel = ("Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that "
+              "show to the people who make shows, and on the strength of that one show they decide if they're going "
+              "to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. "
+              "She starred in one of the ones that became nothing.")
+    Bro = ("Bro ipsum dolor sit amet gaper backside single track, manny Bike epic clipless. Schraeder drop gondy, rail "
+           "fatty slash gear jammer steeps clipless rip bowl couloir bomb hole berm. Huck cruiser crank endo, sucker "
+           "hole piste ripping ACL huck greasy flow face plant pinner. Japan air Skate park big ring trucks shuttle "
+           "stoked rock-ectomy.")
+    Cat = ("Chase ball of string eat plants, meow, and throw up because I ate plants going to catch the red dot today "
+           "going to catch the red dot today. I could pee on this if I had the energy. Chew iPad power cord steal the "
+           "warm chair right after you get up for purr for no reason leave hair everywhere, decide to want nothing to "
+           "do with my owner today.")
+    Cheese = ("Cheese on toast airedale the big cheese. Danish fontina cheesy grin airedale danish fontina taleggio "
+              "the big cheese macaroni cheese port-salut. Edam fromage lancashire feta caerphilly everyone loves "
+              "chalk and cheese brie. Red leicester parmesan cheese and biscuits cheesy feet blue castello cheesecake "
+              "fromage frais smelly cheese.")
+    Bluth = ("A flower in my garden, a mystery in my panties. Heart attack never stopped old Big Bear. I didn't even "
+             "know we were calling him Big Bear. We never had the chance to. Maybe it was the eleven months he spent "
+             "in the womb. The doctor said there were claw marks on the walls of her uterus. Yeah, well, have you "
+             "seen the new Mustang?")
+    Skate = ("Skate ipsum dolor sit amet, alley oop vert mute-air Colby Carter flail 180 berm. Half-cab camel back "
+             "ollie transition ledge Wes Humpston 1080. Carve casper switch kickturn late downhill. Hardware nosebone "
+             "Rick McCrank bluntslide bigspin steps egg plant. Slap maxwell roll-in airwalk fast plant fastplant "
+             "pivot.")
+    Fillerama = ("Tell them I hate them. Is the Space Pope reptilian!? Tell her she looks thin. Hello, little man. "
+                 "I will destroy you! I've got to find a way to escape the horrible ravages of youth. Suddenly, I'm "
+                 "going to the bathroom like clockwork, every three hours. And those jerks at Social Security stopped "
+                 "sending me checks. Now 'I' have to pay them'!")
+    Obama = ("That is the true genius of America - a faith in simple dreams,, an insistence on small miracles. We "
+             "meet at one of those defining moments - a moment when our nation is at war, our economy is in turmoil, "
+             "and the American promise has been threatened once more. We did not go by choice, we went because of "
+             "necessity.")
+    Office = ("Those options are already baked in with this model shoot me an email clear blue water but we need "
+              "distributors to evangelize the new line to local markets, but fire up your browser. Strategic "
+              "high-level 30,000 ft view. Drill down re-inventing the wheel at the end of the day but curate "
+              "imagineer, or to be inspired is to become creative.")
+    Webtwo = ("Webtwo ipsum orkut reddit meebo skype vimeo jajah spock empressr zimbra, mobly napster hipmunk prezi "
+              "chartly bitly spock. Loopt twones meebo hipmunk, fleck xobni. Convore bebo rovio vimeo zanga handango "
+              "blekko koofers, loopt twitter imvu flickr kaboodle chegg. Zillow lala mzinga, sifteo. Voki heekya "
+              "sococo geni oovoo disqus, empressr doostang lijit.")
+    Pirate = ("Lookout flogging bilge rat main sheet bilge water nipper fluke to go on account heave down clap of "
+              "thunder. Reef sails six pounders skysail code of conduct sloop cog Yellow Jack gunwalls grog blossom "
+              "starboard. Swab black jack ahoy Brethren of the Coast schooner poop deck main sheet topmast furl "
+              "marooned.")
+    Sagan = ("Flatland! Hypatia. Galaxies Orion's sword globular star cluster? Light years quasar as a patch of "
+             "light gathered by gravity Vangelis radio telescope. Stirred by starlight vanquish the impossible "
+             "Tunguska event cosmic fugue courage of our questions worldlets galaxies birth colonies. Vastness is "
+             "bearable only through love.")
+    Heisenberg = ("A business big enough that it could be listed on the NASDAQ goes belly up. Disappears! It ceases "
+                  "to exist without me. No, you clearly don't know who you're talking to, so let me clue you in. I "
+                  "am not in danger, Skyler. I AM the danger! A guy opens his door and gets shot and you think that "
+                  "of me? No. I am the one who knocks!")
+    Yolo = ("Yolo ipsum dolor sit amet, consectetur adipiscing elit. Ut ac suscipit leo. Carpe diem vulputate est "
+            "nec commodo rutrum. Pellentesque mattis convallis nisi eu and I ain’t stoppin until the swear jar’s "
+            "full. Ut rhoncus velit at mauris interdum, fringilla dictum neque rutrum. Curabitur mattis odio at erat "
+            "viverra lobortis.")
+    Tuna = ("Rough pomfret lemon shark plownose chimaera southern sandfish kokanee northern sea robin Antarctic cod. "
+            "Yellow-and-black triplefin gulper South American Lungfish mahi-mahi, butterflyfish glass catfish soapfish "
+            "ling gray mullet! Warbonnet denticle herring spiny-back cod straptail tailor zebra loach sea lamprey.")
+    Journo = ("rubber cement algorithms Innovator's Dilemma every dog loves food discuss Flipboard curmudgeon "
+              "afternoon paper content farm Foursquare, WSJ analytics TechCrunch iPad app we will make them pay we "
+              "need a Nate Silver net neutrality incent experiment metered model, going forward Flipboard I love the "
+              "Weather & Opera section information.")
+    Lorizzle = ("Lorem ipsum crackalackin black amet, sizzle adipiscing ass. Bizzle we gonna chung velizzle, pimpin' "
+                "volutpizzle, suscipit brizzle, the bizzle vizzle, arcu. Pellentesque egizzle tortor. Sed erizzle. "
+                "Fusce izzle pizzle dapibizzle turpizzle tempizzle for sure. Maurizzle pellentesque nibh bow wow wow "
+                "izzle. Sizzle izzle tortor.")
+    Farm = ("Storage shed, troughs feed bale manure, is garden wheat oats at augers. Bulls at rose garden cucumbers "
+            "mice sunflower wheat in pig. Chainsaw foal hay hook, herbs at combine harvester, children is mallet. "
+            "Goat goose hen horse. Pick up truck livestock, pets and storage shed, troughs feed bale manure, is "
+            "garden wheat oats at augers. Lamb.")
+    Bavaria = ("Bavaria ipsum dolor sit amet Radler Schneid vui huift vui ognudelt i mechad dee Schwoanshaxn "
+               "Zwedschgndadschi a bissal wos gehd ollaweil. Measi a ganze es i mog di fei aasgem, Blosmusi. "
+               "Schmankal zwoa Ramasuri Edlweiss. Wia vo de Weiznglasl wos, imma hogg di hera Guglhupf! Schorsch "
+               "Spotzerl schnacksln Weiznglasl vui gschmeidig a ganze auf der Oim, da gibt’s koa Sünd, etza!")
+    Placeholder = ("Satoshi Nakamoto launched lots of decentralisation when Litecoin required many decentralised "
+                   "application, for Augur limited some public key behind lots of multi signature. Blockchain thought "
+                   "some robust smart contract in a algorithm! Since OmiseGo bought few double spend, Augur could be "
+                   "many algo-traded vaporware, but Decred data mining few trusted hard fork!")
