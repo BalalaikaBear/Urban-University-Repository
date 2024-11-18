e56fdefae85a0bc4714041f2bcf70d3d345df4f1
BalalaikaBear <eightes@yandex.ru>
2024-11-18 12:41:30 +0300

    None

--- unit16/module_16_1.py
+++ unit16/module_16_1.py
@@ -4,18 +4,18 @@ app = FastAPI()
 
 # fastapi dev unit16/module_16_1.py - запуск файла
 
-@app.get('/')
+@app.get('/')  # при получении запроса типа '/' -> выполнить функцию
 async def main() -> str:
     return 'Главная страница'
 
-@app.get('/user/admin')
+@app.get('/user/admin')  # при получении запроса типа '/user/admin' -> выполнить функцию
 async def admin() -> str:
     return 'Вы вошли как администратор'
 
-@app.get('/user/{user_id}')
+@app.get('/user/{user_id}')  # при получении запроса типа '/user/<текст>' -> выполнить функцию
 async def user_id_func(user_id: str) -> str:
     return f'Вы вошли как пользователь №{user_id}'
 
-@app.get('/user')
+@app.get('/user')  # при получении запроса типа '/user?username=<Имя пользователя>&age=<Возраст>' -> выполнить функцию
 async def user(username: str, age: str) -> str:
     return f'Информация о пользователе: Имя: {username}, Возраст {age}'
